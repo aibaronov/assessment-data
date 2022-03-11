14dcca2e17125d682ed70094d8c6d2230948e994
Arthur Baronov <abaronov1234@gmail.com>
2022-03-11 10:04:09 -0700

    Update process.py

--- process.py
+++ process.py
@@ -4,14 +4,30 @@ log_file = open("um-server-01.txt")
 #creates a function called sales report.
 #This function access the content of the text file
 
+# def sales_reports(log_file):
+#     #this loops through each line of the file using the variable 'line'
+#     for line in log_file:
+#         #strips any trailing character from the end of the string
+#         line = line.rstrip()
+#         #The first three characters of each line are the day of the week
+#         #The variable day access those characters
+#         day = line[0:3]
+#         #Prints the line where the day of the week is Tuesday
+#         if day == "Tue":
+#             print(line)
+
 def sales_reports(log_file):
     #this loops through each line of the file using the variable 'line'
     for line in log_file:
-        #strips any white space from each line
+        #strips any trailing character from the end of the string
         line = line.rstrip()
+        #The first three characters of each line are the day of the week
+        #The variable day access those characters
         day = line[0:3]
-        if day == "Tue":
+        #Prints the line where the day of the week is Tuesday
+        if day == "Mon":
             print(line)
+
 
 
 sales_reports(log_file)
